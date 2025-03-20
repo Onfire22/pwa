29ecc5e10a759a8d91b5d3fc680d1a761db30d0f
maxim necrasov <maddmax22@yandex.ru>
2025-03-20 14:44:51 +0300

    refactored profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import { showNotification } from "../../utils";
 import './style.css';
+import {useNavigate} from "react-router";
 
 const Profile = () => {
+  const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState({
     name: 'admin',
     organization: 't1',
   });
+
+  const handleGoBack = () => {
+    navigate('/');
+  }
 
   const [mode, setMode] = useState('read');
 
@@ -23,36 +29,43 @@ const Profile = () => {
   }
 
   return (
-    <div className="profile">
-      {mode === 'read' ? (
-        <>
-          <p className="profile_name">Имя пользователя: <b>{userInfo.name}</b></p> 
-          <p className="profile_organization">Название организации: <b>{userInfo.organization}</b></p>
-          <button type="button" onClick={() => setMode('edit')}>Редактировать</button>
-        </>
-      ) : (
-        <>
-          <input
-            type="text"
-            placeholder="Имя пользователя"
-            name="name"
-            value={userInput.name}
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="text"
-            placeholder="Название организации"
-            name="organization"
-            value={userInput.organization}
-            onChange={(e) => handleChange(e)}
-          />
-          <button type="button" onClick={() => {
-            setUserInfo(userInput);
-            setMode('read');
-            showNotification('Успешно', 'Данные успешно обновлены', '/icons/check.svg');
-          }}>Сохранить</button>
-        </>
-      )}
+    <div>
+      <button className="btn" onClick={handleGoBack} type="button">
+        Вернуться назад
+      </button>
+      <div className="profile">
+        {mode === 'read' ? (
+          <div className="info">
+            <p className="profile_name">Имя пользователя: <b>{userInfo.name}</b></p>
+            <p className="profile_organization">Название организации: <b>{userInfo.organization}</b></p>
+            <button type="button" onClick={() => setMode('edit')}>Редактировать</button>
+          </div>
+        ) : (
+          <form className="form info">
+            <input
+              className="input"
+              type="text"
+              placeholder="Имя пользователя"
+              name="name"
+              value={userInput.name}
+              onChange={(e) => handleChange(e)}
+            />
+            <input
+              className="input"
+              type="text"
+              placeholder="Название организации"
+              name="organization"
+              value={userInput.organization}
+              onChange={(e) => handleChange(e)}
+            />
+            <button type="button" onClick={() => {
+              setUserInfo(userInput);
+              setMode('read');
+              showNotification('Успешно', 'Данные успешно обновлены', '/icons/check.svg');
+            }}>Сохранить</button>
+          </form>
+        )}
+      </div>
     </div>
   );
 };
