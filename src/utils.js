8fddbbfa9cb3bf228ebebabe30e429a230cdf439
maxim necrasov <maddmax22@yandex.ru>
2025-03-31 14:39:33 +0300

    add push

--- src/utils.js
+++ src/utils.js
@@ -18,6 +18,23 @@ export const showNotification = async (title, body, icon) => {
   });
 }
 
+export const subscribeUser = async (username) => {
+  const registration = await navigator.serviceWorker.register('/sw.js');
+  
+  const subscription = await registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: 'BOKkN_i4LLkDXN1Oqrwa3JJ-37qgvFSsmByL4hVdnroOyDoAvw7lJOXDkU6T619kzd2erRYmIM7Co0sHETM9lWo'
+  });
+  
+  await fetch('https://server-zjg0.onrender.com/subscribe', {
+    method: 'POST',
+    body: JSON.stringify({ username, subscription }),
+    headers: { 'Content-Type': 'application/json' }
+  });
+  
+  console.log(`Пользователь ${username} подписан на push`);
+}
+
 const openDatabase = () => {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open('SyncDB', 1);
